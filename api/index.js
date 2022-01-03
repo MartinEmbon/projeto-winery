16fae64e340c45331c9e70d8acb176541a3bbbe8
Martincho <martinembon@hotmail.com>
2022-01-03 10:12:38 -0300

    api done

--- api/index.js
+++ api/index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv")
 dotenv.config()
 const authRoute = require("./routes/auth")
 const userRoute = require("./routes/users")
+const movieRoute = require("./routes/movies")
+const listRoute = require("./routes/lists")
 
 const connectDB = async () => {
     try{        
@@ -26,6 +28,8 @@ connectDB()
 app.use(express.json())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
+app.use("/api/movies",movieRoute)
+app.use("/api/lists",listRoute)
 
 app.listen(8800,()=>{
     console.log("backend running")
